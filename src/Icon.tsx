6056f93a6bca13e30f919923ed829c12b8383b9b
Walidoux <ma.walidkorchi@icloud.com>
2022-08-22 01:44:13 +0100

    refactor(Icon): clean up props with high default size

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,16 +1,14 @@
-import classNames from 'classnames'
 import React from 'react'
 
 import { IconProps } from './IconProps'
 
-export const Icon: IconProps = ({ children, className, ...rest }) => (
+export const Icon: IconProps = ({ children, ...rest }) => (
   <svg
-    width='35'
-    height='35'
+    width='48'
+    height='48'
     viewBox='0 0 28 28'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
-    className={classNames(className, 'text-black dark:text-white')}
     {...rest}>
     {children}
   </svg>
